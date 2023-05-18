334338843f35c901980ab952c4ccd8f739c9a902
jiloysss <janlloydangeles@gmail.com>
2023-05-18 15:05:49 +0800

    Update

--- face_app/api/after_migrate.py
+++ face_app/api/after_migrate.py
@@ -5,5 +5,9 @@ import os
 def execute():
     print("HERE")
     os.system("bench pip install cmake")
+    os.system("bench pip install opencv-contrib-python")
+    os.system("bench pip install face_recognition")
+    os.system("bench pip install qrcode")
+
     # os.system("ls")
     # os.system("source env/bin/activate")
